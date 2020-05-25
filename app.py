f74a41c880ec66ca98be99d53612a38aaa398aaf
Kirill Arkhipenko <aks1983@mail.ru>
2020-05-25 22:11:00 +0200

    constant parametrisation

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ from itertools import chain
 from tabulate import tabulate
 
 
-class Entry(set):
+class Atom(set):
+    ORDER = 3
+    RANK = ORDER ** 2
+
+
+class Entry(Atom):
+    VALUES = list(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+
     def __init__(self, row, col, box, initial=None):
         self.row = row
         self.col = col
         self.box = box
         if initial is None:
-            super().__init__({1, 2, 3, 4, 5, 6, 7, 8, 9})
+            super().__init__(self.VALUES)
         else:
             super().__init__(initial)
 
@@ -46,17 +53,17 @@ class Entry(set):
     def __str__(self):
         s = ''
         if self.ready:
-            s += '╔═══╗\n║ %d ║\n╚═══╝' % next(iter(self))
+            s += '╔═══╗\n║ %s ║\n╚═══╝' % next(iter(self))
         elif self.empty:
             s = 'X X X\nX X X\nX X X'
         else:
             # s = '...\n...\n...'
-            for i in range(1, 10):
-                if i in self:
-                    s += str(i)
+            for i, v in enumerate(self.VALUES):
+                if v in self:
+                    s += str(v)
                 else:
                     s += '.'
-                if i in {3, 6, 9}:
+                if (i + 1) % self.ORDER == 0:
                     s += '\n'
                 else:
                     s += ' '
@@ -64,7 +71,8 @@ class Entry(set):
         return s
 
 
-class Unit(set):
+class Unit(Atom):
+
     def check(self):
         c = Counter(chain(*self))
         return all(v == 1 for v in c.values())
@@ -116,31 +124,35 @@ class Unit(set):
 
 
 class Row(Unit):
+
     def __str__(self):
         table = sorted(self, key=lambda e: e.col)
         return tabulate([table], tablefmt="orgtbl")
 
 
 class Col(Unit):
+
     def __str__(self):
         table = sorted(self, key=lambda e: e.row)
         return tabulate([[e] for e in table], tablefmt="orgtbl")
 
 
 class Box(Unit):
+
     def __str__(self):
-        table = sorted(self, key=lambda e: e.row * 9 + e.col)
-        return tabulate([[table[r * 3 + c] for c in range(3)] for r in range(3)], tablefmt="orgtbl")
+        table = sorted(self, key=lambda e: e.row * self.RANK + e.col)
+        return tabulate([[table[r * self.ORDER + c] for c in range(self.ORDER)] for r in range(3)], tablefmt="orgtbl")
 
 
-class Board(set):
+class Board(Atom):
+
     def __init__(self, initial=None):
         self.rows = defaultdict(set)
         self.cols = defaultdict(set)
         self.boxs = defaultdict(set)
-        for r in range(9):
-            for c in range(9):
-                box = r // 3 * 3 + c // 3
+        for r in range(self.RANK):
+            for c in range(self.RANK):
+                box = r // self.ORDER * self.ORDER + c // self.ORDER
                 e = Entry(r, c, box, None if initial is None else initial[r][c])
                 self.rows[r].add(e)
                 self.cols[c].add(e)
@@ -148,14 +160,14 @@ class Board(set):
                 self.add(e)
 
     @classmethod
-    def from_array(self, q):
+    def from_array(cls, q):
         initial = []
-        for r in range(9):
+        for r in range(cls.RANK):
             row = []
-            for c in range(9):
-                v = int(q[r][c])
+            for c in range(cls.RANK):
+                v = q[r][c]
                 if v == 0:
-                    v = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+                    v = None
                 else:
                     v = {v}
                 row.append(v)
@@ -163,17 +175,23 @@ class Board(set):
         return Board(initial)
 
     def to_array(self):
-        return [[next(iter(e)) if e.ready else 0 for e in sorted(self.rows[r], key=lambda e: e.col)] for r in range(9)]
+        return [
+            [
+                next(iter(e)) if e.ready else 0
+                for e in sorted(self.rows[r], key=lambda e: e.col)
+            ]
+            for r in range(self.RANK)
+        ]
 
     @classmethod
     def from_string(self, q):
         initial = []
-        for r in range(9):
+        for r in range(self.RANK):
             row = []
-            for c in range(9):
-                v = int(q[r * 9 + c])
-                if v == 0:
-                    v = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            for c in range(self.RANK):
+                v = q[r * self.RANK + c]
+                if v in {'0', ' ', '*'}:
+                    v = None
                 else:
                     v = {v}
                 row.append(v)
@@ -181,10 +199,11 @@ class Board(set):
         return Board(initial)
 
     def to_string(self):
-        return ''.join(str(next(iter(e))) if e.ready else ' ' for e in sorted(self, key=lambda e: e.row * 9 + e.col))
+        return ''.join(
+            str(next(iter(e))) if e.ready else ' ' for e in sorted(self, key=lambda e: e.row * self.RANK + e.col))
 
     def __str__(self):
-        return tabulate([[str(self.row(r))] for r in range(9)], tablefmt="plain")
+        return tabulate([[str(self.row(r))] for r in range(self.RANK)], tablefmt="plain")
 
     def update(self, v, row=None, col=None, box=None):
         es = set(self)
@@ -195,7 +214,6 @@ class Board(set):
         if box is not None:
             es &= self.box[box]
         for e in es:
-            print(v, end='')
             e.clear()
             e.add(v)
 
@@ -212,7 +230,7 @@ class Board(set):
         changed = False
         loop_changed = True
         while loop_changed:
-            for num in range(9):
+            for num in range(self.RANK):
                 loop_changed = False
                 loop_changed |= self.box(num).naked_tuples()
                 loop_changed |= self.row(num).naked_tuples()
@@ -225,7 +243,7 @@ class Board(set):
         loop_changed = True
         while loop_changed:
             loop_changed = False
-            for num in range(9):
+            for num in range(self.RANK):
                 loop_changed |= self.box(num).hidden_tuples()
                 loop_changed |= self.row(num).hidden_tuples()
                 loop_changed |= self.col(num).hidden_tuples()
@@ -237,11 +255,11 @@ class Board(set):
         loop_changed = True
         while loop_changed:
             loop_changed = False
-            for s in range(9):
+            for s in range(self.RANK):
                 box = self.box(s)
-                for num in range(s // 3 * 3, (s // 3 + 1) * 3):
+                for num in range(s // self.ORDER * self.ORDER, (s // self.ORDER + 1) * self.ORDER):
                     loop_changed |= box.intersect(self.row(num))
-                for num in range(s % 3 * 3, (s % 3 + 1) * 3):
+                for num in range(s % self.ORDER * self.ORDER, (s % self.ORDER + 1) * self.ORDER):
                     loop_changed |= box.intersect(self.col(num))
             changed |= loop_changed
         return changed
@@ -253,10 +271,10 @@ class Board(set):
             for v, count in Counter(chain(*items)).items()
             if count == 2
         }
-        for num1 in range(8):
+        for num1 in range(self.RANK - 1):
             row1 = f(self.row(num1))
             col1 = f(self.col(num1))
-            for num2 in range(num1 + 1, 9):
+            for num2 in range(num1 + 1, self.RANK):
                 row2 = f(self.row(num2))
                 col2 = f(self.col(num2))
                 for v in row1.keys() & row2.keys():
@@ -282,13 +300,13 @@ class Board(set):
             for v, count in Counter(chain(*items)).items()
             if count in {2, 3}
         }
-        for num1 in range(7):
+        for num1 in range(self.RANK - 2):
             row1 = f(self.row(num1))
             col1 = f(self.col(num1))
-            for num2 in range(num1 + 1, 8):
+            for num2 in range(num1 + 1, self.RANK - 1):
                 row2 = f(self.row(num2))
                 col2 = f(self.col(num2))
-                for num3 in range(num2 + 1, 9):
+                for num3 in range(num2 + 1, self.RANK):
                     row3 = f(self.row(num3))
                     col3 = f(self.col(num3))
                     for v in row1.keys() & row2.keys() & row3.keys():
@@ -425,21 +443,12 @@ class Board(set):
 
     def check(self):
         c = Counter(chain(*self))
-        return all(v == 9 for v in c.values())
-
-
-def randomize_board(b):
-    # n = random.choice([0, 1, 2, 3, 5, 6, 7, 8])
-    # s = b.row(n)
-    e = random.choice([e for e in b if not e.ready])
-    v = random.choice(list(e))
-    e.difference_update(e - {v})
-    q[e.row][e.col] = v
+        return all(v == self.RANK for v in c.values())
 
 
 def simplify_q(q):
-    for r in range(8, -1, -1):
-        for c in range(8, -1, -1):
+    for r in range(9):
+        for c in range(9):
             v = q[r][c]
             if v == 0:
                 continue
@@ -456,32 +465,66 @@ def simplify_q(q):
 flag = False
 
 if __name__ == '__main__':
-    # X-wing & XY-wing
-    q = [
-        [1, 0, 0, 6, 0, 2, 0, 7, 0],
-        [2, 6, 0, 9, 0, 0, 1, 0, 0],
-        [0, 0, 7, 0, 0, 0, 0, 0, 0],
-        [0, 8, 0, 4, 0, 0, 0, 0, 7],
-        [0, 7, 1, 3, 0, 8, 2, 5, 0],
-        [9, 0, 0, 0, 0, 7, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 4, 0, 0],
-        [0, 0, 4, 0, 0, 6, 0, 2, 3],
-        [0, 2, 0, 1, 0, 4, 0, 0, 6],
-    ]
 
-    a = Board.from_array(q)
+    b = Board()
+    q = b.to_array()
+
+    def randomize_board(q, b):
+        # n = random.choice([0, 1, 2, 3, 5, 6, 7, 8])
+        # s = b.row(n)
+        try:
+            e = random.choice([e for e in b if not e.ready])
+            v = random.choice(list(e))
+        except:
+            print(e)
+            # print(v)
+            raise
+        e.intersect({v})
+        q[e.row][e.col] = v
+
+
+    while not b.solved():
+        randomize_board(q, b)
+        # print(b)
+        b.solve()
+
     b = Board.from_array(q)
-
-    b.solve()
-
-    # while not b.solved():
-    #     randomize_board(b)
-    #     b.solve()
-
-    # simplify_q(q)
-
-    print(b.solved(), b.check())
-    print(tabulate([[a, '-->\n' * 27, b]], tablefmt="plain"))
-
-    print(repr(a.to_string()))
+    print(b)
     print(repr(b.to_string()))
+    b.solve()
+    print(b)
+    #
+    # b = Board.from_string('   5 fe    a 4 7  3     9c4g  d  f    6g3d152 ab a b2  d6 fec g3      f7 b      3d7  1   ga4b6 f  a g528 9c6ed 1  9 3 b6    4 2c  de ba      3   34c   fge  62  g2 f5  1   87 ea      3   9c      f2    a      d8 63  1   2     c9 db  5e        4    d   7f  8 ')
+    # b.solve(True)
+    # print(b)
+
+
+    # # X-wing & XY-wing
+    # q = [
+    #     [1, 0, 0, 6, 0, 2, 0, 7, 0],
+    #     [2, 6, 0, 9, 0, 0, 1, 0, 0],
+    #     [0, 0, 7, 0, 0, 0, 0, 0, 0],
+    #     [0, 8, 0, 4, 0, 0, 0, 0, 7],
+    #     [0, 7, 1, 3, 0, 8, 2, 5, 0],
+    #     [9, 0, 0, 0, 0, 7, 0, 1, 0],
+    #     [0, 0, 0, 0, 0, 0, 4, 0, 0],
+    #     [0, 0, 4, 0, 0, 6, 0, 2, 3],
+    #     [0, 2, 0, 1, 0, 4, 0, 0, 6],
+    # ]
+    #
+    # a = Board.from_array(q)
+    # b = Board.from_array(q)
+    #
+    # b.solve()
+    #
+    # # while not b.solved():
+    # #     randomize_board(b)
+    # #     b.solve()
+    #
+    # # simplify_q(q)
+    #
+    # print(b.solved(), b.check())
+    # print(tabulate([[a, '-->\n' * 27, b]], tablefmt="plain"))
+    #
+    # print(repr(a.to_string()))
+    # print(repr(b.to_string()))
